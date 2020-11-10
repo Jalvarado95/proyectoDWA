db20f2e188791cccd7c2eadde6a452b9a3507151
Jose Ernesto Sanchez Sanchez <jsanchez@lifemiles.loc>
2020-11-10 15:08:49 -0600

    Agregar pagina de contactos

--- ProyectoFinal/js/app.js
+++ ProyectoFinal/js/app.js
@@ -1,10 +1,19 @@
 
-
-
-
 // click abrir cerrar api messenger
 $(".chat-button").on('click', function(e){
     e.preventDefault();
     $(".chat-content").slideToggle('fast');
 });
 
+// funcion para cargar la api de google map
+function iniciarMap(){
+    var coord = {lat:13.674199 ,lng: -89.278822}; //coordenadas
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
