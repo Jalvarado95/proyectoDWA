a5f416570e3b881016a01f6dc73f63ae5f36629c
Napoleón Sáenz <64886543+NapoleonSNZ@users.noreply.github.com>
2020-11-17 01:50:48 -0600

    Agregando Blog, CoheteAnimado

--- ProyectoFinal/js/app.js
+++ ProyectoFinal/js/app.js
@@ -1,10 +1,41 @@
-
-
-
+//variable para capturar coordenadas de usuario
+let map;
 
 // click abrir cerrar api messenger
 $(".chat-button").on('click', function(e){
     e.preventDefault();
     $(".chat-content").slideToggle('fast');
 });
+
+// funcion para cargar la api de google map con las coordenadas de itca
+function iniciarMap(){
+  const coord = {lat:13.674199 ,lng: -89.278822}; //coordenadas de Itca
+  map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+  });
+  const marker1 = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+  marker1.setMap(map);   
+}
+
+//validar si el navegador soporta el api
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(mostrarUbicacion);
+}  
+//obtenemos las coordenadas del usuario
+function mostrarUbicacion(ubicacion) {  
+    const lng = ubicacion.coords.longitude;
+    const lat = ubicacion.coords.latitude;
+
+    console.log(`latitud: ${ lat } | longitud: ${ lng }`);
+    const coord = {lat:lat ,lng: lng}; //coordenadas de usuario
+    const marker2 = new google.maps.Marker({      
+      position: coord, 
+      map: map
+    });    
+    marker2.setMap(map);
+};
 
